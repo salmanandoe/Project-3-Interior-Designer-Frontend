eb2a7017f98412075ff41cc393fe34faf9afd6b3
salmanandoe <123531091+salmanandoe@users.noreply.github.com>
2023-05-31 17:28:30 -0400

    styled home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,13 +3,17 @@
 function Home() {
   return (
     <div className="about-section">
-    <h2>About Me</h2>
+      <h2>GET TO KNOW ME</h2>
 
-    <img className="profile-img" src="https://media.istockphoto.com/id/1212571889/photo/interior-designer-drawing-sketches-while-shopping-at-a-furniture-store.jpg?s=612x612&w=0&k=20&c=mAN5GlExbsTzF4MFeNzQ1C_xusuADGXjoDp4du8iFc8=" />
+      <br />
 
-    <p className='about-me'>
-        I am a passionate interior designer with a flair for creating luxurious spaces that evoke elegance and sophistication. With a keen eye for detail and a deep understanding of aesthetics, I have spent years honing my craft in the realm of high-end design. My portfolio showcases opulent residences that reflect my commitment to excellence. From exquisite furnishings to bespoke finishes, I strive to transform ordinary spaces into extraordinary works of art, leaving a lasting impression on discerning clients around the world.
-    </p>
+      <img className="profile-img" src="https://media.istockphoto.com/id/1212571889/photo/interior-designer-drawing-sketches-while-shopping-at-a-furniture-store.jpg?s=612x612&w=0&k=20&c=mAN5GlExbsTzF4MFeNzQ1C_xusuADGXjoDp4du8iFc8=" />
+
+      <br />
+      
+      <div className="box">
+          <h3>My name is Candace Smith. I am a passionate interior designer with a flair for creating luxurious spaces that evoke elegance and sophistication. With a keen eye for detail and a deep understanding of aesthetics, I have spent years honing my craft in the realm of high-end design. My portfolio showcases opulent residences that reflect my commitment to excellence. From exquisite furnishings to bespoke finishes, I strive to transform ordinary spaces into extraordinary works of art, leaving a lasting impression on discerning clients around the world.</h3>
+      </div>
     </div>
   )
 }
