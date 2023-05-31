67b9dfff0fdc2c0e25471c804f66e19770c1e542
salmanandoe <123531091+salmanandoe@users.noreply.github.com>
2023-05-30 22:42:31 -0400

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import './components/stylesheet.css';
+import './stylesheet.css';
 import { Routes, Route } from "react-router-dom";
-import Body from './components/Body';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Home from './Pages/Home';
@@ -22,24 +21,6 @@ function App() {
           </Routes>
       <Footer />
     </div>
-
-    
-    // <div className="App">
-    //   <header className="App-header">
-    //     <p>
-    //       <img src="https://media.istockphoto.com/id/1308282338/photo/modern-bathroom-interior-stock-photo.jpg?s=612x612&w=0&k=20&c=r6qXSudX7P7YOzYsuHsQ_6pjAekJOF4XnEpdrtfqmsg=" />
-    //     </p>
-  
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Welcome
-    //     </a>
-    //   </header>
-    // </div>
 
   );
 }
